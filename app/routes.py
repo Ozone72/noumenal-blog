ba9174bf50a49133422a4100202d5adbd74c6563
Orin Fletcher <orinf@yahoo.com>
2018-09-19 19:50:38 -0700

    basic templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,31 @@
+from flask import render_template
 from app import app_var
 
 @app_var.route('/')
 @app_var.route('/index')
 def index():
-    return "Hello, Coach Pie!"
+    user = {'username': 'Coach Pie'}
+    posts = [
+        {
+            'author': {'username':'Orin'},
+            'body': 'A Crisp fall day in Seattle'
+        },
+        {
+            'author': {'username': 'Vincent'},
+            'body': 'Another day at werk...'
+        },
+        {
+            'author': {'username': 'Josh'},
+            'body': 'Money for nothing and my chicks for free :)'
+        },
+        {
+            'author': {'username': 'Jed'},
+            'body': 'MY band'
+        },
+        {
+            'author': {'username': 'Brett'},
+            'body': 'I love whiskey!'
+        }
+        
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
