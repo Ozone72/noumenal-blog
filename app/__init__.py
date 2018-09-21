f9b7a70586531b61491b7ad15f72f55fbe88645f
Orin Fletcher <orinf@yahoo.com>
2018-09-20 18:50:55 -0700

    sqlite database creation

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app_var = Flask(__name__)
 app_var.config.from_object(Config)
+db = SQLAlchemy(app_var)
+migrate = Migrate(app_var, db)
 
-from app import routes
+
+from app import routes, models
