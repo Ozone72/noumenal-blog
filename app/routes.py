8be33a8b4f31852e3bf53854470bffdcddec7c7c
Orin Fletcher <orinf@yahoo.com>
2018-09-22 14:56:00 -0700

    first branch commit; added profile route

--- app/routes.py
+++ app/routes.py
@@ -74,3 +74,14 @@ def register():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app_var.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
